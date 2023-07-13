c2bb1f90d9101ad43e757db6a6cd2a8d4e535f64
Sahil Pipaliya <114761517+Sahil7698@users.noreply.github.com>
2023-07-13 13:10:15 +0530

    Update quote_model.dart

--- lib/model/quote_model.dart
+++ lib/model/quote_model.dart
@@ -1,16 +1,19 @@
 class Quote {
-  int? id;
+  int id;
   late final String quote;
+  late final String type;
 
   Quote({
-    this.id,
+    required this.id,
     required this.quote,
+    required this.type,
   });
 
   factory Quote.fromMap({required Map<String, dynamic> data}) {
     return Quote(
       id: data['Id'],
       quote: data['Name'],
+      type: data['type'],
     );
   }
 }
