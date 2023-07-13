f5541db95f34648ea17ce4f3bacc4672c81dd6c5
Sahil Pipaliya <114761517+Sahil7698@users.noreply.github.com>
2023-07-13 18:02:08 +0530

    Update quote_helper.dart

--- lib/controllers/helpers/quote_helper.dart
+++ lib/controllers/helpers/quote_helper.dart
@@ -2,7 +2,6 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 import '../../model/quote_model.dart';
-import '../../res/quote_list.dart';
 
 class DBHelper {
   DBHelper._();
@@ -21,26 +20,33 @@ class DBHelper {
       onCreate: (Database db, int ver) async {
         String query =
             "CREATE TABLE IF NOT EXISTS tbl_quote(Id INTEGER PRIMARY KEY AUTOINCREMENT,quote TEXT NOT NULL,type TEXT NOT NULL);";
+        // String queryHistory =
+        //     "CREATE TABLE IF NOT EXISTS tbl_history(Id INTEGER PRIMARY KEY AUTOINCREMENT,quote TEXT NOT NULL,type TEXT NOT NULL);";
         await db.execute(query);
+        //await db.execute(queryHistory);
       },
     );
   }
 
-  Future<int> insertRecord() async {
+  Future<int> insertRecord(
+      {required String quote, required String type}) async {
     await initDB();
 
-    String query = "INSERT INTO tbl_quote(Id,quote,type) VALUES(?,?,?);";
-    List args = [
-      Global.alone.length,
-      Global.angry.length,
-      Global.happy.length,
-      Global.attitude.length,
-      Global.life.length,
-      Global.time.length,
-    ];
+    String query = "INSERT INTO tbl_quote(quote,type) VALUES(?,?);";
+    List args = [quote, type];
 
     return await db!.rawInsert(query, args);
   }
+
+  // Future<int> insertHistory(
+  //     {required String quote, required String type}) async {
+  //   await initDB();
+  //
+  //   String queryHistory = "INSERT INTO tbl_history(quote,type) VALUES(?,?);";
+  //   List args = [quote, type];
+  //
+  //   return await db!.rawInsert(queryHistory, args);
+  // }
 
   Future<List<Quote>> getAllRecords({required String type}) async {
     await initDB();
@@ -49,7 +55,18 @@ class DBHelper {
     List<Map<String, dynamic>> allRecords = await db!.rawQuery(query);
 
     List<Quote> allQuotes =
-    allRecords.map((e) => Quote.fromMap(data: e)).toList();
+        allRecords.map((e) => Quote.fromMap(data: e)).toList();
     return allQuotes;
   }
+
+  // Future<List<Quote>> getAllHistory() async {
+  //   await initDB();
+  //   String query = "SELECT * FROM tbl_history;";
+  //
+  //   List<Map<String, dynamic>> allHistories = await db!.rawQuery(query);
+  //
+  //   List<Quote> allQuotes =
+  //       allHistories.map((e) => Quote.fromMap(data: e)).toList();
+  //   return allQuotes;
+  // }
 }
