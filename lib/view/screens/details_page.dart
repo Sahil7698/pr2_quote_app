f9ee52bcfca62c110ddf2da979605cff4ea4d0ae
Sahil Pipaliya <114761517+Sahil7698@users.noreply.github.com>
2023-07-13 13:09:16 +0530

    Update details_page.dart

--- lib/view/screens/details_page.dart
+++ lib/view/screens/details_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
+import '../../controllers/helpers/quote_helper.dart';
+
 class DetailPage extends StatefulWidget {
   final String title;
   final List data;
+
   const DetailPage({
     Key? key,
     required this.title,
@@ -14,7 +17,7 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
-  int q = 0;
+  int q = 1;
 
   void changeTime() {
     for (int i = 0; i < widget.data.length; i++) {
@@ -22,8 +25,6 @@ class _DetailPageState extends State<DetailPage> {
         setState(() {
           q = i;
         });
-        // int totalHistory = Global.history.length;
-        // Global.history[totalHistory + 1]['quote'] = widget.data[i]['quote'];
         changeTime();
       });
     }
@@ -32,8 +33,8 @@ class _DetailPageState extends State<DetailPage> {
   @override
   void initState() {
     super.initState();
-
     changeTime();
+    DBHelper.dbHelper.getAllRecords(type: widget.title);
   }
 
   @override
@@ -41,6 +42,10 @@ class _DetailPageState extends State<DetailPage> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
+        leading: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pop();
+            }, child: const Icon(Icons.arrow_back_ios_new)),
         title: Text(
           widget.title,
           style: const TextStyle(
@@ -49,35 +54,47 @@ class _DetailPageState extends State<DetailPage> {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Container(
-          alignment: Alignment.center,
-          height: 700,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            color: Colors.red.shade300,
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(15),
-              child: Column(
-                children: List.generate(
-                  1,
-                  (index) => Text(
-                    "${widget.data[q]['quote']}",
-                    style: const TextStyle(
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold,
+      body: FutureBuilder(
+          future: DBHelper.dbHelper.getAllRecords(type: widget.title),
+          builder: (context, snapShot) {
+            if (snapShot.hasError) {
+              return Text(
+                "${snapShot.hasError}",
+              );
+            } else if (snapShot.hasData) {
+              var data = snapShot.data;
+              return Padding(
+                padding: const EdgeInsets.all(15),
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 700,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.red.shade300,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Column(
+                        children: List.generate(
+                          1,
+                              (index) => Text(
+                            data![q].quote,
+                            style: const TextStyle(
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ).toList(),
+                      ),
                     ),
                   ),
-                ).toList(),
-              ),
-            ),
-          ),
-        ),
-      ),
+                ),
+              );
+            }
+            return const Center(child: CircularProgressIndicator());
+          }),
     );
   }
 }
