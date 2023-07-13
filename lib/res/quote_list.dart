169a8630712fabcf7b99bd519c63110a9453b8eb
Sahil Pipaliya <114761517+Sahil7698@users.noreply.github.com>
2023-07-13 13:10:36 +0530

    Update quote_list.dart

--- lib/res/quote_list.dart
+++ lib/res/quote_list.dart
@@ -4,24 +4,32 @@ class Global {
       'id': '1',
       'quote':
           'Fail I alone, in words and deeds? Why, all men strive and who succeeds?',
+      'type' : 'alone'
     },
     {
       'id': '2',
       'quote': 'The strong man is strongest when alone.',
+      'type' : 'alone',
     },
     {
       'id': '3',
       'quote':
           'Loneliness expresses the pain of being alone and solitude expresses the glory of being alone.',
+      'type' : 'alone',
+
     },
     {
       'id': '4',
       'quote':
           'I think Alone in the Dark was too much an action creature movie than a horror creature movie.',
+      'type' : 'alone',
+
     },
     {
       'id': '5',
       'quote': 'To live alone is the fate of all great souls.',
+      'type' : 'alone',
+
     },
   ];
 
@@ -30,23 +38,33 @@ class Global {
       'id': '1',
       'quote':
           'Don’t waste your time in anger, regrets, worries, and grudges. Life is too short to be unhappy.',
+      'type' : 'angry',
+
     },
     {
       'id': '2',
       'quote':
           'Bitterness is like cancer. It eats upon the host. But anger is like fire. It burns it all clean.',
+      'type' : 'angry',
+
     },
     {
       'id': '3',
       'quote': 'When angry, count four. When very angry, swear.',
+      'type' : 'angry',
+
     },
     {
       'id': '4',
       'quote': 'A heart filled with anger has no room for love.',
+      'type' : 'angry',
+
     },
     {
       'id': '5',
       'quote': 'Angry people are not always wise.',
+      'type' : 'angry',
+
     },
   ];
 
@@ -55,24 +73,34 @@ class Global {
       'id': '1',
       'quote':
           'People wait all week for Friday, all year for summer, all life for happiness',
+      'type' : 'happy',
+
     },
     {
       'id': '2',
       'quote': 'Happiness adds and multiplies, as we divide it with others.',
+      'type' : 'happy',
+
     },
     {
       'id': '3',
       'quote':
           'The foolish man seeks happiness in the distance; The wise grows it under his feet.',
+      'type' : 'happy',
+
     },
     {
       'id': '4',
       'quote':
           'Happiness is a perfume you cannot pour on others without getting a few drops on yourself.',
+      'type' : 'happy',
+
     },
     {
       'id': '5',
       'quote': 'The key to happiness is good health and a bad memory',
+      'type' : 'happy',
+
     },
   ];
 
@@ -81,24 +109,34 @@ class Global {
       'id': '1',
       'quote':
           'Xcuse me, I found something under my shoes. Oh its your attitude.',
+      'type' : 'attitude',
+
     },
     {
       'id': '2',
       'quote': 'When i was born..Devil said..?Oh Shit..!! Competition?',
+      'type' : 'attitude',
+
     },
     {
       'id': '3',
       'quote':
           'I enjoy when people show Attitude to me because it shows that they need an Attitude to impress me!',
+      'type' : 'attitude',
+
     },
     {
       'id': '4',
       'quote': 'Smile in front of people who hate you Ur happiness kills them',
+      'type' : 'attitude',
+
     },
     {
       'id': '5',
       'quote':
           'Someone Asked me what is UR attitude then i simply replied ? BEING SINGLE IS MY ATTITUDE',
+      'type' : 'attitude',
+
     },
   ];
 
@@ -107,25 +145,33 @@ class Global {
       'id': '1',
       'quote':
           'Every story has an end, but in life every ending is just a new beginning.',
+      'type' : 'life',
+
     },
     {
       'id': '2',
       'quote': 'Never stop learning, because life never stops teaching.',
+      'type' : 'life',
+
     },
     {
       'id': '3',
       'quote':
           'Kill tension before tensions kill you, reach your goal before goal kicks you, live life before life leaves you.',
+      'type' : 'life',
+
     },
     {
       'id': '4',
       'quote':
           'You have to fight through some bad days to earn the best days of your life.',
+      'type' : 'life',
     },
     {
       'id': '5',
       'quote':
           'No one said life would be easy, they just promised it would get better.',
+      'type' : 'life',
     },
   ];
 
@@ -134,24 +180,34 @@ class Global {
       'id': '1',
       'quote':
           'Time spent with family is time well spent. Family is the one constant element in life.',
+      'type' : 'time',
+
     },
     {
       'id': '2',
       'quote':
           'Time and good friends are two things that become more valuable the older you get.',
+      'type' : 'time',
+
     },
     {
       'id': '3',
       'quote': 'Time is the wisest counselor of all.',
+      'type' : 'time',
+
     },
     {
       'id': '4',
       'quote':
           'Time is the school in which we learn, time is the fire in which we burn.',
+      'type' : 'time',
+
     },
     {
       'id': '5',
       'quote': 'Time is a great healer, but a poor beautician.',
+      'type' : 'time',
+
     },
   ];
 
